90cc48862eac716789217723d66bf0bb1003da4d
Ismael G Costa <notmymail@notmymail.com>
2024-05-17 02:10:28 -0300

    teste unitários

--- api-infra-oauth/api-infra-oauth-core/api-infra-oauth-use-case/src/test/java/br/com/ismyburguer/core/auth/usecase/impl/client/ApiGatewayFeignClientTest.java
+++ api-infra-oauth/api-infra-oauth-core/api-infra-oauth-use-case/src/test/java/br/com/ismyburguer/core/auth/usecase/impl/client/ApiGatewayFeignClientTest.java
@@ -1,15 +1,16 @@
 package br.com.ismyburguer.core.auth.usecase.impl.client;
 
+import br.com.ismyburguer.core.adapter.out.FeignClientAPI;
 import br.com.ismyburguer.core.auth.gateway.out.LambdaClientOAuthSignIn;
 import br.com.ismyburguer.core.auth.gateway.out.LambdaOAuthSignUp;
 import br.com.ismyburguer.core.auth.gateway.out.OAuth2ClientCredentialsFeignInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.verifyNoInteractions;
@@ -25,11 +26,12 @@ public class ApiGatewayFeignClientTest {
 
     @BeforeEach
     public void setUp() {
+        apiGatewayFeignClient = new ApiGatewayFeignClient(interceptor);
         apiGatewayFeignClient.setApiGateway("http://example.com");
     }
 
     @Test
-    public void testCreateClient() {
+    void testCreateClient() {
         // Arrange
         Class<LambdaOAuthSignUp> apiType = LambdaOAuthSignUp.class;
 
@@ -41,7 +43,7 @@ public class ApiGatewayFeignClientTest {
     }
 
     @Test
-    public void testCreateClientWithInterceptor() {
+    void testCreateClientWithInterceptor() {
         // Arrange
         Class<LambdaClientOAuthSignIn> apiType = LambdaClientOAuthSignIn.class;
 
